bf45aba53cda0ff69f4ac1a641072c0b9b18da19
Rene <raoever@gmail.com>
2022-11-09 19:21:29 -0300

    Commit Inicial.

---
+++ src/main/java/dio/parking/DIOParking/DioParkingApplication.java
@@ -0,0 +1,13 @@
+package dio.parking.DIOParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/parking/DIOParking/DioParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.parking.DIOParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
