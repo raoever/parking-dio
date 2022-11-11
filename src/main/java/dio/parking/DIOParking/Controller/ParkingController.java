b23501effdb81885d6cbc612ed1b9ceedb5751bc
Rene <raoever@gmail.com>
2022-11-10 23:55:36 -0300

    Commit Controller Update.

--- src/main/java/dio/parking/DIOParking/Controller/ParkingController.java
+++ src/main/java/dio/parking/DIOParking/Controller/ParkingController.java
@@ -1,10 +1,13 @@
 package dio.parking.DIOParking.Controller;
 
 import dio.parking.DIOParking.Model.Parking;
+import dio.parking.DIOParking.Repository.ParkingRepository;
 import dio.parking.DIOParking.Service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
@@ -13,8 +16,16 @@ public class ParkingController {
     @Autowired
     private ParkingService parkingService;
 
+    @Autowired
+    private ParkingRepository parkingRepository;
+
     public ParkingController(ParkingService parkingService) {
         this.parkingService = parkingService;
+    }
+
+    @GetMapping
+    public List<Parking> findAll(){
+        return parkingRepository.findAll();
     }
 
     @PostMapping
